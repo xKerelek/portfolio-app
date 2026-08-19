1b6782659d765ddf29983753c2b4e20ade027e92
Karol Nowak <karolnowak3217@gmail.com>
2026-08-19 18:33:21 +0200

    Complete HomeComponent

--- portfolio-frontend/src/app/app.routes.ts
+++ portfolio-frontend/src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
-import {AppComponent} from './app.component';
-import {AboutComponent} from './features/about/about.component';
-import {ContactComponent} from './features/contact/contact.component';
-import {HomeComponent} from './features/home/home.component';
+import { AboutComponent } from './features/about/about.component';
+import { ContactComponent } from './features/contact/contact.component';
+import { HomeComponent } from './features/home/home.component';
+import { ProjectsComponent } from './features/projects/projects.component';
 
 export const routes: Routes = [
-  {path: '', component: HomeComponent, pathMatch: 'full'},
-  {path: 'about', component: AboutComponent},
-  {path: 'contact', component: ContactComponent},
-
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'projects', component: ProjectsComponent },
 ];
